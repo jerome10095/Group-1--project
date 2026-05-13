808ad3e563ee4325d6a03d481ec1722d3130c050
Jerome Cyiza <jeromecyiza@gmail.com>
2026-05-13 10:57:10 +0200

    //saved and committed changes to index.html, style.css, and script.js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-const navLinks = document.getElementsByClassName("nav")
+const navLinks = document.getElementsByTagName('li');
 
 for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("mouseover", function() {
-        this.style.backgroundColor = "lightgray";
-    });
-
-    navLinks[i].addEventListener("mouseout", function() {
-        this.style.backgroundColor = "";
+    navLinks[i].addEventListener('mouseover', function() {
+        navLinks[i].style.color = 'grey';
     });
 }
+
+ document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const products = document.querySelectorAll(".product");
+
+    searchInput.addEventListener("keyup", function () {
+        const searchValue = searchInput.value.toLowerCase();
+
+        products.forEach(function (product) {
+            const productName = product.querySelector("h2").textContent.toLowerCase();
+
+            if (productName.includes(searchValue)) {
+                product.style.display = "block";
+            } else {
+                product.style.display = "none";
+            }
+        });
+    });
+});
+
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const closeBtn = document.getElementById("closeBtn");
+const slideNav = document.getElementById("slideNav");
+const overlay = document.getElementById("overlay");
+
+hamburgerBtn.addEventListener("click", function () {
+    slideNav.classList.add("open");
+    overlay.classList.add("show");
+});
+
+closeBtn.addEventListener("click", function () {
+    slideNav.classList.remove("open");
+    overlay.classList.remove("show");
+});
+
+overlay.addEventListener("click", function () {
+    slideNav.classList.remove("open");
+    overlay.classList.remove("show");
+});
